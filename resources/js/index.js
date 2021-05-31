3846e80a73c52726532d215b39f93b10601b2294
Fahim Foysal <ffoysal6@gmail.com>
2021-05-31 21:26:30 +0600

    conditional-dynamic data

--- resources/js/index.js
+++ resources/js/index.js
@@ -7,6 +7,27 @@ data() {
         productImage : './resources/image/space-tour.jpg',
         productImageAlt : 'Space Tour',
         productImageStyle : 'responsive-image centered-image',
+
+        productClasses: [
+            {
+                name: 'Coach',
+                price: 125000,
+                seatsAvailable: 20,
+                earlyBird: true
+            },
+            {
+                name: 'Business',
+                price: 275000,
+                seatsAvailable: 6,
+                earlyBird: true
+            },
+            {
+                name: 'First',
+                price: 430000,
+                seatsAvailable: 2,
+                earlyBird: false
+            }
+        ]
     }
 }
 }).mount('#app');
